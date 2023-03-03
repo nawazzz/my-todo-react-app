ee5a0db924659196370ecb8e864a332dd0043872
nawaz <khan.nawaz790@gmail.com>
2023-03-03 15:30:32 +0100

    create todo react app

---
+++ App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>My ToDo App</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
